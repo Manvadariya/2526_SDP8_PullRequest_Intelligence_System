b8dde2d36fd73b10643acb90887bac7a665503a7
shubham-vaishnav-13 <vaishnavshubham857@gmail.com>
2025-12-08 16:35:13 +0530

    Add Tailwind CSS integration with configuration and initial setup

---
+++ AgenticPR/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+     <div className="text-4xl font-bold text-blue-600">
+      Tailwind v4 working! 🚀
+    </div>
+  )
+}
+
+export default App
